bf3711543da6ea2de104a78450f19a606c07e3fe
DhananjaiNirmal <43343190+DhananjaiNirmal@users.noreply.github.com>
2020-08-06 15:37:03 +0530

    Add files via upload

---
+++ Snake/src/Point.java
@@ -0,0 +1,30 @@
+public class Point {
+ private int x,y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(int i) {
+        x=0;
+        y=0;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
